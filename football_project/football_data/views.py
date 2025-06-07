f0ba8d3566b1ae25108e61f99f779bf6ed52e52a
Andres-Gerena-ITC <afgerenac@itc.edu.co>
2025-06-07 10:45:15 -0500

    CRUD Team
    CRUD para Teams con control de usuarios y permisos

--- football_project/football_data/views.py
+++ football_project/football_data/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics, permissions
+from .models import Team
+from .serializers import TeamSerializer
+from .permissions import IsAdminUser
+
+# Lista todos los equipos
+class TeamListView(generics.ListAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Crear equipo
+class TeamCreateView(generics.CreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdminUser]
+
+# Editar un equipo
+class TeamUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdminUser]
+    lookup_field = 'id'
+
+# Eliminar un equipo
+class TeamDeleteView(generics.DestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdminUser]
+    lookup_field = 'id'
