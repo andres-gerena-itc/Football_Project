bc095db06c4edf58f04bee2476a11f9f97c75455
Fabián Suarez <fesuareze@itc.edu.co>
2025-06-09 07:12:06 -0500

    Add import required

--- football_project/football_data/tests.py
+++ football_project/football_data/tests.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.permissions import BasePermission
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Team
 from .models import Match
