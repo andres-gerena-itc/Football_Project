b55522ef5da38537d55eee2b0a36ed209d50fb6c
Fabián Suarez <fesuareze@itc.edu.co>
2025-06-09 07:11:33 -0500

    Trying a test

--- football_project/football_data/tests.py
+++ football_project/football_data/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import Team
+from .models import Match
 
-# Create your tests here.
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['id', 'name']
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data["groups"] = [group.name for group in self.user.groups.all()]
+        return data
+    
+class IsGuestUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.groups.filter(name='invitado').exists()
