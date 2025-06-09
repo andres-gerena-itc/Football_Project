871dd72436702cb7615966a65b4659963c8de52a
Fabián Suarez <fesuareze@itc.edu.co>
2025-06-09 11:44:19 -0500

    Test models

--- football_project/football_data/api.py
+++ football_project/football_data/api.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Match
+from .models import Match, Team
 
 @api_view(['GET'])
 def kpi_summary(request):
--- football_project/football_data/models.py
+++ football_project/football_data/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
-    country = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=255, null=False, blank=False)
 
 class Match(models.Model):
     home_team = models.ForeignKey(Team, related_name='home_matches', on_delete=models.CASCADE)
     away_team = models.ForeignKey(Team, related_name='away_matches', on_delete=models.CASCADE)
     date = models.DateTimeField()
-    home_score = models.IntegerField(null=True, blank=True)
-    away_score = models.IntegerField(null=True, blank=True)
-    competition = models.CharField(max_length=100, null=True, blank=True)
-    stage = models.CharField(max_length=100, null=True, blank=True)
+    home_score = models.IntegerField(null=False, blank=False)
+    away_score = models.IntegerField(null=False, blank=False)
+    competition = models.CharField(max_length=100, null=False, blank=False)
+    stage = models.CharField(max_length=100, null=False, blank=False)
